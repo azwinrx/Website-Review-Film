4b55e6d8c9ac8b003ccd55da47a9aac6181ba98f
azwinrx <azwinrifai321@gmail.com>
2025-02-24 01:41:11 +0700

    everything looks fine, tinggal atur font, color, hover, made layout responsive with media, and etc

--- js/script.js
+++ js/script.js
@@ -15,3 +15,37 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     });
 });
 
+//Slide Image Gameplay
+const images = [
+    "assets/gameplay-1.png",
+    "assets/gameplay-2.png",
+    "assets/gameplay-3.png",
+    "assets/gameplay-4.png",
+    "assets/gameplay-5.png",
+    "assets/gameplay-6.png",
+    "assets/gameplay-7.png",
+    "assets/gameplay-8.png",
+];
+  
+  let currentIndex = 0;
+  
+  function showSlide(index) {
+    const imgElement = document.getElementById("gameplay-image");
+    if (index >= images.length) {
+      currentIndex = 0;
+    } else if (index < 0) {
+      currentIndex = images.length - 1;
+    } else {
+      currentIndex = index;
+    }
+    imgElement.src = images[currentIndex];
+  }
+  
+  function nextSlide() {
+    showSlide(currentIndex + 1);
+  }
+  
+  function prevSlide() {
+    showSlide(currentIndex - 1);
+  }
+  
